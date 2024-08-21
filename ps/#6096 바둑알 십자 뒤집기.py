f1e173a29686c9204a281af6b5f8ef85fd577268
Seunghwan Lee <80394375+lsh4867@users.noreply.github.com>
2024-08-21 18:30:55 +0900

    Update and rename #1.py to #6096 바둑알 십자 뒤집기.py

---
+++ ps/#6096 바둑알 십자 뒤집기.py
@@ -0,0 +1,26 @@
+# list comprehension: 2차원 리스트 초기화 form
+d = [[0] * 20 for _ in range(20)]
+
+# 2차원 리스트 값 입력 form
+for i in range(1,20):
+  a = list(map(int, input().split()))
+  for j in range(1,20):
+    d[i][j] = a[j-1]
+
+# 핵심!: 바둑알 뒤쥡기 method
+n = int(input())
+for _ in range(n):
+  x, y = map(int, input().split())
+  for j in range(1,20):
+    d[x][j] = 1 - d[x][j]
+    d[j][y] = 1 - d[j][y]
+
+# 2차원 리스트 출력 form(1)
+for i in range(1, 20):
+  print(' '.join(map(str, d[i][1:20])))
+
+# 2차원 리스트 출력 form(2-정석)
+for i in range(1, 20) :
+  for j in range(1, 20) :
+    print(d[i][j], end=' ')
+  print()
